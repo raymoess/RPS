b643eaedb254a98f0fd3b8c09219a3c41c9336fe
raymoess <raymond9382@gmail.com>
2024-11-28 21:18:37 -0500

    Created two more function.
    Created a function that will track the amount of round plays and a function to run the game itself.

--- javascript.js
+++ javascript.js
@@ -6,3 +6,14 @@ function getComputerChoice(){
 function getUserChoice(){
 
 }
+
+let userScore = 0;
+let computerScore = 0;
+
+function rpsRound(userChoice, computerChoice){
+
+}
+
+function playRPS(){
+    
+}
