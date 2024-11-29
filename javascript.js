98c0e4120907ea4dccbac0320ecb26a6bb183b61
raymoess <raymond9382@gmail.com>
2024-11-29 11:13:35 -0500

    Round function complete.
    The round function uses if statements to verify whether or not the user or computer wins. If the user inputs an invalid option, an error message will be thrown.

--- javascript.js
+++ javascript.js
@@ -1,30 +1,59 @@
-function getComputerChoice(){
-    let options = ['rock' , 'paper', 'scissors'];
-    num = Math.floor(Math.random() * 3);
-    let result = options[num];
+function getComputerChoice() {
+  let options = ["rock", "paper", "scissors"];
+  num = Math.floor(Math.random() * 3);
+  let result = options[num];
 
-    return result;
+  return result;
 }
 
-function getUserChoice(str){
-
-    userChoice = prompt("Please enter a choice between: Rock, Paper, or Scissors: ");
-    while (typeof userChoice !== "str"){
-        choice = prompt("Please enter a choice between: Rock, Paper, or Scissors: ");
-    }
-    userChoice = userChoice.toLowerCase();
-    return userChoice
-
+function getUserChoice() {
+  userChoice = prompt(
+    "Please enter a choice between: Rock, Paper, or Scissors: "
+  );
+  while (typeof userChoice !== "string") {
+    choice = prompt(
+      "Please enter a choice between: Rock, Paper, or Scissors: "
+    );
+  }
+  userChoice = userChoice.toLowerCase();
+  return userChoice;
 }
 
-let options = ['rock' , 'paper', 'scissors']
 let userScore = 0;
 let computerScore = 0;
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
 
-function rpsRound(userChoice, computerChoice){
+function rpsRound(userChoice, computerChoice) {
 
+  if (userChoice === computerChoice) {
+    console.log(`You both chose ${computerChoice}, it is a tie!`);
+ } else if (
+    (userChoice === rock && computerChoice === scissors) ||
+    (userChoice === paper && computerChoice === rock) ||
+    (userChoice === scissors && computerChoice === paper)) {
+    console.log(`You win, your opponent chose ${computerChoice}!`);
+    computerScore++;
+    
+  } else if (
+    (computerChoice === rock && userChoice === scissors) ||
+    (computerChoice === paper && userChoice === rock) ||
+    (computerChoice === scissors && userChoice === paper)
+  ) {
+    console.log(`You lose, your opponent chose ${computerChoice}!`);
+    userScore++;
+
+  } else {
+    console.log(`Invalid input, the choice you have provided is neither of the options.`);
+  }
 }
+const userPick = getUserChoice();
+const computerPick = getComputerChoice();
 
-function playRPS(){
+console.log(rpsRound(userPick, computerPick));
 
+function playRPS() {
+
+    
 }
