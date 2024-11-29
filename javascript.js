c9798674a39704b4b71bf20f0de8164a101ad325
raymoess <raymond9382@gmail.com>
2024-11-28 21:43:09 -0500

    Finished "getComputerChoice" function.
    We created an array with the available choice. We then created a variable that stores the a randomly generated value between 0 - 2. After which we get our result by using that random value to access the index of our array.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,18 @@
 function getComputerChoice(){
+    let options = ['rock' , 'paper', 'scissors'];
+    num = Math.floor(Math.random() * 3);
+    let result = options[num];
 
-
+    return result;
 }
 
 function getUserChoice(){
 
+    
+
 }
 
+let options = ['rock' , 'paper', 'scissors']
 let userScore = 0;
 let computerScore = 0;
 
@@ -15,5 +21,5 @@ function rpsRound(userChoice, computerChoice){
 }
 
 function playRPS(){
-    
+
 }
